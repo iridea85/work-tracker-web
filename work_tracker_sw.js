1f4b71e13263e517877c646f088242ae24801c75
iridea85 <iridea85@gmail.com>
2026-09-18 11:36:42 -0700

    Publish compiled Work Tracker PWA

---
+++ work_tracker_sw.js
@@ -0,0 +1,82 @@
+const CACHE_PREFIX = 'work-tracker-pwa-';
+const CACHE_NAME = `${CACHE_PREFIX}2`;
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './flutter_bootstrap.js',
+  './main.dart.js',
+  './assets/AssetManifest.bin',
+  './assets/AssetManifest.bin.json',
+  './assets/FontManifest.json',
+  './assets/fonts/MaterialIcons-Regular.otf',
+  './assets/fonts/fallback/Roboto-Regular.ttf',
+  './assets/shaders/ink_sparkle.frag',
+  './assets/shaders/stretch_effect.frag',
+  './canvaskit/canvaskit.js',
+  './canvaskit/canvaskit.wasm',
+  './canvaskit/chromium/canvaskit.js',
+  './canvaskit/chromium/canvaskit.wasm',
+  './favicon.png',
+  './icons/Icon-192.png',
+  './icons/Icon-512.png',
+  './icons/Icon-maskable-192.png',
+  './icons/Icon-maskable-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      Promise.all(
+        APP_SHELL.map((url) => cache.add(url).catch(() => null))
+      )
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      });
+    })
+  );
+});
